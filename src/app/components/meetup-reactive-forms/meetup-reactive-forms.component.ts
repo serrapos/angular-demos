ede17f141810fbc390a32611b722f63422a69ee3
Sergio Raposo Vargas <sraposov@everis.com>
2019-01-23 18:50:59 +0100

    Limpieza ficheros

--- src/app/components/meetup-reactive-forms/meetup-reactive-forms.component.ts
+++ src/app/components/meetup-reactive-forms/meetup-reactive-forms.component.ts
@@ -7,38 +7,9 @@ import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms'
 })
 export class MeetupReactiveFormsComponent implements OnInit {
 
-  formDemo = this.formBuilder.group({
-    'username': ['', [Validators.required, Validators.pattern('[a-zA-Z]*'), Validators.minLength(5)]],
-    'password': ['', Validators.required],
-    'repitePassword': ['', Validators.required],
-    'email': ['', [Validators.required, Validators.email, this.validateFormatEmail]],
-    'fullname': ['', Validators.required]
-  });
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor() { }
 
   ngOnInit() {
-    this.formDemo.setValidators(this.validatePassword);
-  }
-
-  formSubmit () {
-    console.log('Formulario -> ', this.formDemo.value);
-  }
-
-  validateFormatEmail(control: FormControl) {
-    if (control.value.endsWith('.com') || control.value.endsWith('.es')) {
-      return null;
-    } else {
-      return {'extensionError': true};
-    }
-  }
-
-  validatePassword(group: FormGroup) {
-    if (group.value.password === group.value.repitePassword) {
-      return null;
-    } else {
-      return {'repitePasswordError': true};
-    }
   }
 
 }
